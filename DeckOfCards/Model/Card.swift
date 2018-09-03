70d02ef6e6daa156b37dd0220c35a6c2242ea2f7
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-03 11:08:42 -0600

    Added Card model

---
+++ DeckOfCards/Model/Card.swift
@@ -0,0 +1,30 @@
+//
+//  Deck.swift
+//  DeckOfCards
+//
+//  Created by John Tate on 9/3/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+import Foundation
+
+
+struct DeckDictionary: Decodable {
+
+    // This property is on the top level of the JSON
+    // MARK: - Properties
+    let cards: [Card]
+    
+    // We don't have to use this because 'cards' matches the JSON
+//    private enum: CodingKeys: String, CodingKey {
+//        case cards = "cards"
+//
+//    }
+    
+    struct Card: Decodable {
+        
+        let value: String
+        let suit: String
+        let image: String
+    }
+}
